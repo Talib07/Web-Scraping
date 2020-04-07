9deaa721493abb18c45318374db287c7e849f0e7
Talib07 <43445227+Talib07@users.noreply.github.com>
2020-04-08 02:24:01 +0530

    Update NewsScraper.py

--- NewsScraper.py
+++ NewsScraper.py
@@ -16,27 +16,32 @@ description = description.getText().strip()
 time = top_news.find('span', class_="gs-u-vh")
 time = time.getText().strip()
 location = top_news.find('a',class_="gs-c-section-link gs-c-section-link--truncate nw-c-section-link nw-o-link nw-o-link--no-visited-state")
-location = location.getText().strip()
+location = location.getText().strip()for _ in classes:
+    news = results.find_all('div',class_= _)
+    
+
 print('Title:' + str(title),end = '\n')
 print('Description:'+ str(description),end = "\n")
 print('Time:' + str(time),end = "\n")
 print('Location or Domain:' + str(location),end = "\n\n\n")
 
+classes = ["gel-layout__item gs-u-pb+@m gel-1/3@m gel0-1/4@xl gel-1/3@xxl nw-o-keyline nw-o-no-keyline@m","gel-layout__item gs-u-pb+@m gel-1/3@m gel-1/4@xl gel-1/3@xxl nw-o-keyline nw-o-no-keyline@m"]
 
-news = results.find_all('div',class_="gel-layout__item gs-u-pb+@m gel-1/3@m gel-1/4@xl gel-1/3@xxl nw-o-keyline nw-o-no-keyline@m")
-
-for single_news in news:
-    title = single_news.find('h3',class_ = "gs-c-promo-heading__title gel-pica-bold nw-o-link-split__text")
-    description = single_news.find('p',class_ = "gs-c-promo-summary gel-long-primer gs-u-mt nw-c-promo-summary gs-u-display-none gs-u-display-block@m")
-    time = single_news.find("span",class_ = 'gs-u-vh')
-    location = single_news.find('a',class_ = "gs-c-section-link gs-c-section-link--truncate nw-c-section-link nw-o-link nw-o-link--no-visited-state")
-    new = [title,description,time,location]
-
-    title = title.getText().strip()
-    description = description.getText().strip()
-    time = time.getText().strip()
-    location = location.getText().strip()
-    print('Title:' + str(title),end = '\n')
-    print('Description:'+ str(description),end = "\n")
-    print('Time:' + str(time),end = "\n")
-    print('Location or Domain:' + str(location),end = "\n\n\n")
+for _ in classes:
+    news = results.find_all('div',class_= _)
+    
+    for single_news in news:
+        title = single_news.find('h3',class_ = "gs-c-promo-heading__title gel-pica-bold nw-o-link-split__text")
+        description = single_news.find('p',class_ = "gs-c-promo-summary gel-long-primer gs-u-mt nw-c-promo-summary gs-u-display-none gs-u-display-block@m")
+        time = single_news.find("span",class_ = 'gs-u-vh')
+        location = single_news.find('a',class_ = "gs-c-section-link gs-c-section-link--truncate nw-c-section-link nw-o-link nw-o-link--no-visited-state")
+        new = [title,description,time,location]
+        
+        title = title.getText().strip()
+        description = description.getText().strip()
+        time = time.getText().strip()
+        location = location.getText().strip()
+        print('Title:' + str(title),end = '\n')
+        print('Description:'+ str(description),end = "\n")
+        print('Time:' + str(time),end = "\n")
+        print('Location or Domain:' + str(location),end = "\n\n\n")
